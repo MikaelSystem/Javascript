86d628cd05176e872880b680cd9a2675490017eb
Mikael Oliveira <Mikaelroblox076@gmail.com>
2026-07-11 11:26:18 -0300

    Menu aberto e fechar programa.

--- Básico/CalculadoraBasica.js
+++ Básico/CalculadoraBasica.js
@@ -1,4 +1,4 @@
-let res,alternativa;
+let res, menu = true;
 
 function soma(){
     let v1 = Number(prompt("Digite o valor que deseja somar: ")) 
@@ -29,41 +29,44 @@ function resto(){
     return v1%v2
 }
 
-console.log("======================")
-console.log("[1] - Soma")
-console.log("[2] - Multiplicar")
-console.log("[3] - Dividir")
-console.log("[4] - Resto da Divisão")
-console.log("======================")
-    let opcao = prompt("Escolha uma opção: ")
+while (menu){
+    console.log("======================")
+    console.log("[1] - Soma")
+    console.log("[2] - Multiplicar")
+    console.log("[3] - Dividir")
+    console.log("[4] - Resto da Divisão")
+    console.log("[5] - Sair do Programa")
+    console.log("======================")
+        let opcao = prompt("Escolha uma opção: ")
     
-console.clear();
-
-switch(opcao){
-    case "1":
-        res = soma()
-            console.clear();
-                console.log("O resultado da soma é de", res,"!") 
-        break
-    case "2":
-        res = multiplicar()
-            console.clear();
-                console.log("O resultado da multiplicação é de", res,"!")
-        break
-    case "3":
-        res = dividir()
-            console.clear();
-            if (res===null){
-                console.log("Erro, não é possível dividir por 0!")
-            }else{
-                console.log("O resultado da divisão é de", res,"!")
-            }
-                
-        break
-    case "4":
-        res = resto()
-            console.clear();
-                console.log("O resto da divisão é de", res,"!")
-        break
-        
+    switch(opcao){
+        case "1":
+            res = soma()
+                console.clear();
+                    console.log("O resultado da soma é de", res,"!") 
+            break
+        case "2":
+            res = multiplicar()
+                console.clear();
+                    console.log("O resultado da multiplicação é de", res,"!")
+            break
+        case "3":
+            res = dividir()
+                console.clear();
+                if (res===null){
+                    console.log("Erro, não é possível dividir por 0!")
+                }else{
+                    console.log("O resultado da divisão é de", res,"!")
+                }
+                    
+            break
+        case "4":
+            res = resto()
+                console.clear();
+                    console.log("O resto da divisão é de", res,"!")
+            break
+        case "5":
+            menu = false
+    }
 }
+
